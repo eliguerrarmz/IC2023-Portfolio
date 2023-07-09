18a099936d541a50f628e81d90cdfb47fee88644
eliguerrarmz <guerraeli76@gmail.com>
2023-07-08 23:02:56 -0400

    added contact info and linked

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-// var navbar = document.getElementById("navbar")
-// var menu = document.getElementById("menu")
+let git = document.querySelector(".git")
 
-// window.onscroll = function(){
-//     if(window.pageYOffset >= menuoffsetTop){
-//         navbar.classList.add("sticky");
-//     }else{
-//         navbar.classList.remove("sticky");
-//     }
-    
-// }
