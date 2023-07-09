0337948fed7d13200deada25ed6cac7a197fa4c1
eliguerrarmz <guerraeli76@gmail.com>
2023-07-08 22:02:23 -0400

    STICKY NAVBAR :+1:

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// var navbar = document.getElementById("navbar")
+// var menu = document.getElementById("menu")
 
+// window.onscroll = function(){
+//     if(window.pageYOffset >= menuoffsetTop){
+//         navbar.classList.add("sticky");
+//     }else{
+//         navbar.classList.remove("sticky");
+//     }
+    
+// }
